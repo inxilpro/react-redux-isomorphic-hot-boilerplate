bbf6abf699b68102ad77981db7b6f444dbfbeeda
Chris Morrell <inxilpro@users.noreply.github.com>
2015-10-07 14:56:04 -0400

    Updated webpack devtool to `cheap-module-eval-source-map`

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -4,7 +4,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	devtool: 'eval',
+	devtool: 'cheap-module-eval-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
 		path.join(__dirname, '..', 'client', 'src', 'app')
